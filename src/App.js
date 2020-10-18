7e970a70b2bae8e050cbc6b9f020e9171ab8012d
Benjamin Stewart <benjamin.stewart@duke.edu>
2020-10-17 23:19:46 -0400

    Getting some styling to happen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import styles from './arts.module.css';
 
+import styles from './arts.module.css';
 import './components/LoFi/script';
 
-function App() {
-  return (
-    <div className={styles.container}>
-      Hi
-    </div>
-  );
+const STARTER_BACKGROUND = 'https://res.cloudinary.com/dhzssvuhz/image/upload/v1602988324/drizzle/henrik-evensen-winter-forest_dyb1es.jpg';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div className={styles.regularBody} style={{ backgroundImage: `url(${STARTER_BACKGROUND})` }}>
+        <div className={styles.temp}>
+          72°
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/start.js
+++ src/start.js
@@ -9,6 +9,8 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 600,
+    minHeight: 300,
     webPreferences: {
       nodeIntegration: true,
       webSecurity: false,
