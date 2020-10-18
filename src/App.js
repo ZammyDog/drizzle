5fce55907fd838dcef2e37de02ad825bad7ca968
Benjamin Stewart <benjamin.stewart@duke.edu>
2020-10-18 01:33:24 -0400

    Adding time and date

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
 import React from 'react';
 import classNames from 'classnames';
+import axios from 'axios';
+import moment from 'moment';
 
 import styles from './arts.module.css';
 // import './components/LoFi/script';
 
 const STARTER_BACKGROUND = 'https://res.cloudinary.com/dhzssvuhz/image/upload/v1602988324/drizzle/henrik-evensen-winter-forest_dyb1es.jpg';
+const OPEN_WEATHER_MAP_API_KEY = 'abc60186fcef4077d4da248e9e804e3d';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      ready: false,
+      temperature: 0,
+      time: '',
+      timeAdd: '',
+      dateStr: '',
     };
   }
 
   componentDidMount() {
-    console.log(Navigator);
+    // get out current coordinates
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const coords = pos.coords;
+      // get the weather at our coordinates
+      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coords.latitude}&lon=${coords.longitude}&units=imperial&exclude=alerts&appid=${OPEN_WEATHER_MAP_API_KEY}`)
+        .then((response) => {
+          // update our ui with the weather and time
+          this.setState({
+            ready: true,
+            time: moment().format('h:mm'),
+            timeAdd: moment().format('a'),
+            dateStr: moment().format('dddd, MMMM Do, YYYY'),
+            temperature: Math.round(response.data.current.temp),
+          }, () => {
+            // now potentially update the time every second (so we don't miss a minute)
+            this.timeInterval = setInterval(() => {
+              const timeData = moment();
+              this.setState({
+                time: timeData.format('h:mm'),
+                timeAdd: timeData.format('a'),
+                dateStr: timeData.format('dddd, MMMM Do, YYYY'),
+              });
+            }, 1000);
+          });
+        })
+        .catch((err) => console.error(err));
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeInterval);
   }
 
   render() {
@@ -24,9 +62,24 @@ class App extends React.Component {
         <div className={styles.titleBar}>
           <i className={classNames('fas fa-times', styles.closeIcon)} aria-label="Close" role="button" tabIndex={0} onClick={window.close} />
         </div>
-        <div className={styles.temp}>
-          72°
-        </div>
+
+        {this.state.ready ? (
+          <>
+            <div className={styles.time}>
+              <div style={{ height: 86 }}>
+                {this.state.time}
+                <span style={{ fontSize: 24 }}>{this.state.timeAdd}</span>
+              </div>
+              <div className={styles.dateText}>
+                {this.state.dateStr}
+              </div>
+            </div>
+
+            <div className={styles.temperature}>
+              {`${this.state.temperature}°`}
+            </div>
+          </>
+        ) : null}
       </div>
     );
   }
